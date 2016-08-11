1ddb6cc4a73a3e6e7d0b984f905237723ab9146f
Ksenia <Ksenia>
2016-08-11 18:02:12 -0400

    made some changes

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -162,6 +162,46 @@ nomadApp.displayData = function(finalResult) {
 			return eachCity.scores.free_wifi_available * 100;
 		});
 
+		Handlebars.registerHelper('months', function() {
+			eachCity.info.monthsToVisit.map(function() {
+				if (month === 1) {
+					month = 'January';
+				}
+				else if (month === 2) {
+					month = 'February';
+				}
+				else if (month === 3) {
+					month = 'March';
+				} 
+				else if (month === 4) {
+					month = 'April';
+				}
+				else if (month === 5) {
+					month = 'May';
+				} 
+				else if (month === 6) {
+					month = 'June';
+				}
+				else if (month === 7) {
+					month = 'July';
+				}
+				else if (month === 8) {
+					month = 'August';
+				}
+				else if (month === 9) {
+					month = 'September';
+				}
+				else if (month === 10) {
+					month = 'October';
+				}
+				else if (month === 11) {
+					month = 'November';
+				}
+				else if (month === 12) {
+					month = 'December';
+				}
+		});
+
 		var finalTemplate = template(eachCity);
 		$("#result").append(finalTemplate);
 
